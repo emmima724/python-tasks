bda0a03c050ec6f38a96d40ef8c6c3aa82f5c350
emmima724 <emmimasanthosh627@gmail.com>
2025-05-12 14:11:23 +0530

    Add files via upload

---
+++ personalized greeting card generator.py
@@ -0,0 +1,3 @@
+print("My name isEmmima")
+print("My favourite quote is Let us pick up our books and our pens. They are our most powerful weapons — Malala Yousafzai")
+print(" my hobbies is Exploring Food")
