22a470225f7add428671e42460d50bc75bee2e35
emmima724 <emmimasanthosh627@gmail.com>
2025-05-14 22:29:40 +0530

    Add files via upload

---
+++ largest of the  three number.py
@@ -0,0 +1,12 @@
+print("largest of 3 numbers")
+num1=int(input("enter the 1st number:"))
+num2=int(input("enter the 2nd number:"))
+num3=int(input("enter the 3rd number:"))
+if num1>num2 and num1>num3:
+    print(f"{num1} iS greater")
+elif num2>num1 and num2>num3:
+    print(f"{num2} is greater")
+elif num3>num1 and num3>num2:
+    print(f"{num3} is greater")
+
+
